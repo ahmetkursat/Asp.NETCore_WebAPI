a6e9aff255edb9432af0c57c85a0e9d8bcdca09d
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-09-22 00:36:49 +0300

    dotnet cli üzerinden projeyi oluşturduk Models klasöründe projedeki objeleri oluşturduk repository context klasöründe ise Dbcontext ile veritabanı baglantıların yapacagımız dbset ile modelini oluşturdugumuz nesnenin Dbset kullanarak set ettik sonra nuget paketlerinden efcore efcore desing efcore sqlserver efcore tools paketlerini indirdik program.cs de builder.services adddbcontext servisini ekledik generic list oldugu için RepositoryContext nesnemizi verdik options lar ekleyerek fat function kullanarak sql stringleri oluşturduk en sonda dbsetini contextini ayarladıgmızın nesnelerin migratesini ayarladık add-mig mig ismi  sonra update database diyerek nesneleri tablolara cevirdik veri tabanında kontrol ettik

---
+++ bsStoreApp/WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options):base(options)
+        {
+            
+
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
