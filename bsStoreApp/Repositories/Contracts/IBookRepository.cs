ca6c14b41a8f323518f70d8a22749ce250aa7f88
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-09-23 19:40:30 +0300

    kitaplar için ınterface tanımladık ve o interface i base interfaceinden kalıtım yaptık Book interface ,için kendine özgü Id cagırma methodu yazdık

---
+++ bsStoreApp/Repositories/Contracts/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+
+namespace Repositories.Contracts
+{
+    public interface IBookRepository :IRepositoryBase<Book>
+    {
+        IQueryable<Book> GetAllBooks(bool trackChanges);
+        IQueryable<Book> GetOneBookById(bool trackChanges,int id);
+        void CreateOneBook(Book book);
+        void UpdateOneBook(Book book);
+        void DeleteOneBook(Book book);
+
+
+    }
+}
---
+++ bsStoreApp/Repositories/EfCore/BookRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Repositories.Contracts;
+
+namespace Repositories.EfCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+            
+
+        }
+
+        public IQueryable<Book> GetAllBooks(bool trackChanges) => FindAll(trackChanges);
+
+
+        public IQueryable<Book> GetOneBookById(bool trackChanges, int id) => FindByCondition(b => b.Id.Equals(id),trackChanges).OrderBy(b => b.Id);
+
+
+        public void CreateOneBook(Book book) => Create(book);
+
+
+        public void UpdateOneBook(Book book) => Update(book);
+
+
+        public void DeleteOneBook(Book book) => Delete(book);
+
+    }
+}
