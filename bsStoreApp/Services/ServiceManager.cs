ff7f2cad8330a20e40ddc0d885a9f2c636d73510
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-09-25 22:13:38 +0300

    book servis için Service manageri oluşturduk

---
+++ bsStoreApp/Services/ServiceManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IBookService> _bookService;
+        public ServiceManager(IRepositoryManager repositoryManager)
+        {
+            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager));
+        }
+
+        public IBookService BookService => _bookService.Value;
+    }
+}
