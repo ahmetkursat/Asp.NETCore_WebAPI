10ce041741e1fbf9c5fbe99c85e7aa03d0b3cfb3
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-09-29 19:47:45 +0300

    Repository Context Factory oluşturduk dbcontext ve options ayarlarını yaptık

---
+++ bsStoreApp/WebApi/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Repositories.EfCore;
+
+namespace WebApi.ContextFactory
+{
+    public class RepositoryContextFactory :IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            //configuration
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            //DbContextOptionsBuilder
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>().UseSqlServer(
+                    configuration.GetConnectionString("sqlConnection"),prj => prj.MigrationsAssembly("WebApi"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
---
+++ bsStoreApp/WebApi/Migrations/20230929163951_startPoint.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class startPoint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, 75m, "Hacivat karagöz" },
+                    { 2, 175m, "mesnevi" },
+                    { 3, 375m, "devlet" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
