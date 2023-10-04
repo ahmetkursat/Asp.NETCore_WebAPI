745cc3a0c67f2aea9650bb5c0b2d7d15fa72023d
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-10-04 16:14:37 +0300

    Nlog Configursayon yaptık nuget paketleridnen ekledik servis extensions da tanımladık program cs te build ettik AddSingleton methodunu kullandık

--- bsStoreApp/WebApi/Program.cs
+++ bsStoreApp/WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using NLog;
 using Repositories.EfCore;
 using WebApi.Extensions;
 
@@ -6,6 +7,7 @@ using WebApi.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(),"/Nlog.config"));
 
 builder.Services.AddControllers().AddApplicationPart(typeof(Presentation.AssemblyReferance).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +17,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureLoggerManager();
+
 
 var app = builder.Build();
 
