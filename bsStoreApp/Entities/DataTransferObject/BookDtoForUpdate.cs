69e9a35874acf19656edf746dceb28acc083c5dc
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-10-07 20:35:52 +0300

    Entity üzerinde datatransferobject klasörü oluşturduk veritabanından gelen verileri mapledik

---
+++ bsStoreApp/Entities/DataTransferObject/BookDtoForUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObject
+{
+    public record BookDtoForUpdate(int id, String Title, decimal Price);
+
+    //{
+    //public int Id { get; init; }
+
+    //public String Title { get; init; } Dtoları bu şekilde de tanımlayabilir
+
+    //public decimal Price { get; init; }
+    //}
+}
